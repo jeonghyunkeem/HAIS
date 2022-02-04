6985b9f4ed184fb4f77300712459e2e42e498f61
Jeonghyun Kim <jeonghyunkeem@github.com>
2022-02-04 08:23:11 +0000

    modify to be adapted to CUDA 11

--- lib/hais_ops/setup.py
+++ lib/hais_ops/setup.py
@@ -1,6 +1,8 @@
+import os
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+os.environ["TORCH_CUDA_ARCH_LIST"] = "3.7+PTX;5.0;6.0;6.1;6.2;7.0;7.5"
 setup(
     name='HAIS_OP',
     ext_modules=[
@@ -9,7 +11,7 @@ setup(
 
             'src/hais_ops.cpp',
             'src/cuda.cu'
-        ], extra_compile_args={'cxx': ['-g'], 'nvcc': ['-O2']})
+        ], extra_compile_args={'cxx': ['-g'], 'nvcc': ['-O3']})
     ],
     cmdclass={'build_ext': BuildExtension}
 )
